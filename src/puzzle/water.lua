8fdcff0a3cf559cc4220b8edaee75deecc4cb5d8
Brooks J Rady <b.j.rady@gmail.com>
2018-02-18 03:10:44 -0700

    FUCK YA! WATER PHYSICS!

--- src/puzzle/water.lua
+++ src/puzzle/water.lua
@@ -21,7 +21,7 @@ tankColor = {200,200,200}
 waterColor = {30, 50, 180}
 backgroundColor = {75, 75, 75}
 tankThickness = 5
-waterRate = 1000
+waterRate = 500
 openingWidth = 20
 
 waterSX = nil
@@ -35,9 +35,9 @@ function Water.init()
   puzzle = {
     A = {x=570, y=30, w=80, h=60, lid=false, opns={{side="B", pos=0.5, open=true, link="P1"}, {side="L", pos=0.8, open=true, link="P4"}}, v=0},
     P1 = {x=600, y=90, w=openingWidth, h=25, lid=true, opns={{side="T", pos=0.5, open=true, link="A"}, {side="B", pos=0.5, open=true, link="B"}}, v=0},
-    B = {x=570, y=115, w=80, h=60, lid=false, opns={{side="R", pos=0.2, open=true, link="P2"}}, v=0},
-    P2 = {x=650, y=127, w=100, h=openingWidth, lid=true, opns={{side="L", pos=0.5, open=true, link="B"}, {side="B", pos=1.0, open=true, link="C"}}, v=0},
-    P3 = {x=730, y=147, w=openingWidth, h=28, lid=true, opns={{side="T", pos=0.5, open=true, link="P2"}, {side="B", pos=0.5, open=true, link="C"}}, v=0},
+    B = {x=570, y=115, w=80, h=60, lid=false, opns={{side="T", pos=0.5, open=true, link="P1"}, {side="R", pos=0.2, open=true, link="P2"}}, v=0},
+    P2 = {x=650, y=123, w=100, h=openingWidth, lid=true, opns={{side="L", pos=0.5, open=true, link="B"}, {side="B", pos=1.0, open=true, link="P3"}}, v=0},
+    P3 = {x=730, y=143, w=openingWidth, h=32, lid=true, opns={{side="T", pos=0.5, open=true, link="P2"}, {side="B", pos=0.5, open=true, link="C"}}, v=0},
     C = {x=710, y=175, w=60, h=60, lid=true, opns={{side="T", pos=0.5, open=true, link="P3"}}, v=0},
     D = {x=400, y=100, w=80, h=60, lid=false, opns={{side="R", pos=0.2, open=true, link="P6"}}, v=0},
     P4 = {x=535, y=62, w=35, h=openingWidth, lid=true, opns={{side="L", pos=0.5, open=true, link="P5"}, {side="R", pos=0.5, open=true, link="A"}}, v=0},
@@ -51,25 +51,42 @@ local function waterLevel(tank)
 end
 
 local function isFull(tank)
-  if tank.lid then
-    return waterLevel(tank) < tank.h - tankThickness
-  else
-    return waterLevel(tank) < tank.h
-  end
+  return waterLevel(tank) > tank.h - tankThickness and tank.lid
 end
 
 local function activeOutlets(tank)
   active = {}
   for _,opn in pairs(tank.opns) do
-    if opn.side == "B" then
-      table.insert(active,opn.link)
-    elseif opn.side == "T" and tank.lid and waterLevel(tank) > tank.h - tankThickness then
-      table.insert(active,opn.link)
-    elseif (opn.side == "R" or opn.side == "L") and waterLevel(tank) > tank.h * (1 - opn.pos) - openingWidth / 2 then
-      table.insert(active,opn.link)
+    if opn.open and tank.v > 0 then
+      if opn.side == "B" then
+        table.insert(active,opn.link)
+      elseif opn.side == "T" and tank.lid and waterLevel(tank) >= tank.h - tankThickness then
+        table.insert(active,opn.link)
+      elseif (opn.side == "R" or opn.side == "L") and waterLevel(tank) > (tank.h - openingWidth) * (1 - opn.pos) then
+        table.insert(active,opn.link)
+      end
     end
   end
   return active
+end
+
+local function flow(tank, link, depth, dt)
+  if depth > 10 then
+    return nil
+  end
+  if link.y + link.h - waterLevel(link) > tank.y + tank.h - waterLevel(tank) and tank.v > 0 and not isFull(link) then
+    tank.v = tank.v - waterRate * dt
+    if tank.v < 0 then
+      link.v = link.v + (tank.v + waterRate * dt)
+      tank.v = 0
+    else
+      link.v = link.v + waterRate * dt
+    end
+  elseif link.y + link.h - waterLevel(link) > tank.y + tank.h - waterLevel(tank) and tank.v > 0 then
+    for id,link2 in pairs(activeOutlets(link)) do
+      flow(tank, puzzle[link2], depth + 1, dt)
+    end
+  end
 end
 
 function Water.runPuzzle(dt)
@@ -80,10 +97,7 @@ function Water.runPuzzle(dt)
       state = 8
     end
     for id,link in pairs(activeOutlets(tank)) do
-      if puzzle[link].y - waterLevel(puzzle[link]) > tank.y - waterLevel(tank) and tank.v > 0 and isFull(puzzle[link]) then
-        tank.v = tank.v - waterRate * dt
-        puzzle[link].v = puzzle[link].v + waterRate * dt
-      end
+      flow(tank, puzzle[link], 0, dt)
     end
   end
 end
@@ -110,8 +124,8 @@ function Water.drawTank(tank)
   for _,opn in pairs(tank.opns) do
     if opn.open then
       love.graphics.setColor(backgroundColor)
-      --[[if util.containsValue(activeOutlets(tank),opn.link) and tank.v > 0 then
-        love.graphics.setColor(waterColor)
+      --[[if util.containsValue(activeOutlets(tank),opn.link) then
+        love.graphics.setColor(0, 255, 0)
       end--]]
     else
       love.graphics.setColor(tankColor)
